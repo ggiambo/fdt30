d7af23ae5e91af085a0381f398e3bdfd9d2020bd
giambo <ggiambo@gmail.com>
2021-04-25 11:23:17 +0200

    Look&Feel

--- frontend/src/Main/Content/Content.js
+++ frontend/src/Main/Content/Content.js
@@ -5,23 +5,27 @@ import {Route} from 'react-router-dom';
 import Login from "./Login";
 import Messages from "./Messages";
 import Register from "./Register";
+import {Container} from "react-bootstrap";
+import styles from './Content.module.css';
 
 const Content = () => {
     return (
-        <Switch>
-            <Route exact path={"/message"}>
-                <Message/>
-            </Route>
-            <Route path={"/messages/:pageNr?"}>
-                <Messages/>
-            </Route>
-            <Route exact path={"/login"}>
-                <Login/>
-            </Route>
-            <Route exact path={"/register"}>
-                <Register/>
-            </Route>
-        </Switch>
+        <Container className={styles.content}>
+            <Switch>
+                <Route exact path={"/message"}>
+                    <Message/>
+                </Route>
+                <Route path={"/messages/:pageNr?"}>
+                    <Messages/>
+                </Route>
+                <Route exact path={"/login"}>
+                    <Login/>
+                </Route>
+                <Route exact path={"/register"}>
+                    <Register/>
+                </Route>
+            </Switch>
+        </Container>
     );
 }
 
