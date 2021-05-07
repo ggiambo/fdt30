d8169483d931af40587211eb378c400d80f6b294
Gabriele Giambonini <gabriele.giambonini@axa.ch>
2021-05-07 11:39:53 +0200

    Springboot 2.5.0-RC1

--- src/test/kotlin/net/rcfmedia/fdt30/Fdt30ApplicationTests.kt
+++ src/test/kotlin/net/rcfmedia/fdt30/Fdt30ApplicationTests.kt
@@ -2,8 +2,11 @@ package net.rcfmedia.fdt30
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Fdt30ApplicationTests {
 
     @Test
